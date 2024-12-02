f319f633c5e82d28a0c18b90903706e2d4e8786a
AlexaGabriel <alexandre181828@gmail.com>
2024-12-02 20:41:14 -0300

    init

---
+++ src/App/index.ts
@@ -0,0 +1,20 @@
+import fastify from "fastify";
+import fastifyCors from "@fastify/cors";
+
+const app = fastify({logger: true});
+
+app.register(fastifyCors)
+
+app.get('/', async (request, reply) => {
+    reply.send({ message: 'Hello, Fastify!' });
+  });
+
+const start = async () => {
+    try {
+        await app.listen({ port: 3000 })
+    } catch (error) {
+        app.log.error(error);
+        process.exit(1);
+    }
+}
+start()
